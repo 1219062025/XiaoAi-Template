7a2133e42b226361a78525334a47e294c4664ef5
1219062025 <1219062025@qq.com>
2022-07-18 21:22:38 +0800

    init

--- vue-ts-default/src/utils/logics/clientBridge.ts
+++ vue-ts-default/src/utils/logics/clientBridge.ts
@@ -1,26 +1,27 @@
 import { getParams } from '@eyunmy/bridge';
 import { useStore } from '@/store';
 import { getParameterByName } from '@/lib';
+import type { Params } from '@eyunmy/bridge/types';
 
 export const clientBridge = async () => {
   const store = useStore();
 
   try {
-    const data = await getParams();
-    const { uid = '', token = '', channel = '', model = '' } = data || {};
-    const { uid: tuid } = getParameterByName(['uid']);
+    const data = (await getParams()) as Params;
+    const { uid = '', token = '', channel = '', model = '' } = data;
+    const { uid: tuid = '' } = getParameterByName(['uid']);
 
     const isGoogle = channel === 'GooglePlay';
     let isiPhoneX = false;
     if (model.indexOf('iPhone1') !== -1) {
       isiPhoneX = true;
     }
-    tuid && store.setState('tuid', tuid);
+    store.setState('tuid', tuid);
     store.setState('uid', uid);
     store.setState('token', token);
     store.setState('isiPhoneX', isiPhoneX);
     store.setState('isGoogle', isGoogle);
   } catch (error) {
-    console.log(error);
+    console.log('Not on the client. Ignore this information in the test environment');
   }
 };
